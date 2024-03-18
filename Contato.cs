04018c156ca1df2a9d8c8be7b310ac2887cd6e53
Jônatas <jonatas.07022003@gmail.com>
2024-03-18 08:09:09 -0300

    Add files via upload

---
+++ Contato.cs
@@ -0,0 +1,14 @@
+﻿namespace Agenda
+{
+    public class Contato
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+    }
+
+    public static class ContatosManager
+    {
+        public static List<Contato> Contatos { get; } = new List<Contato>();
+    }
+}
