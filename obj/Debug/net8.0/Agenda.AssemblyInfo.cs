8720526e31921527732a506f76978136b4e77b13
Jonatas <jonatas.07022003@gmail.com>
2024-03-18 10:01:17 -0300

    Ajustando detalhes de estilos

--- obj/Debug/net8.0/Agenda.AssemblyInfo.cs
+++ obj/Debug/net8.0/Agenda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agenda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04018c156ca1df2a9d8c8be7b310ac2887cd6e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80d26c70563d67ca8971e845ea04ee7a90b595e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agenda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agenda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
